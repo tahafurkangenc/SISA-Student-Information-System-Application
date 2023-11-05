22a96038d8a5c0b05b67261a00ea0abc61a6ee3c
Taha Furkan Genç <tahafurkangenc@gmail.com>
2023-11-05 07:18:52 +0300

    öğrenci Frame'i kısalttım :D

--- KayitUygulamasiv2/OgrenciForm.Designer.cs
+++ KayitUygulamasiv2/OgrenciForm.Designer.cs
@@ -166,7 +166,7 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1347, 627);
+            this.ClientSize = new System.Drawing.Size(1347, 402);
             this.Controls.Add(this.talepgonderButton);
             this.Controls.Add(this.talepgondertabloyenilebutton);
             this.Controls.Add(this.dersadifltreButton);
